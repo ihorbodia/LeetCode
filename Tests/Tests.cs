163103cf1050db516b23e5e2ca32eb8d46420594
Ihor Bodia <ihorbodia@gmail.com>
2023-07-18 00:36:34 +0200

    605. Can Place Flowers

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -6,13 +6,13 @@ public class Tests
     [InlineData(new[] {1,0,0,0,1}, 1, true)]
     [InlineData(new[] {1,0,0,0,1}, 2, false)]
     [InlineData(new[] {0,0,0,0,0,1}, 1, true)]
+    [InlineData(new[] {0,0,1,0,1}, 1, true)]
     public void CanPlaceFlowers_605(int[] flowerbed, int n, bool expectedResult)
     {
         for (int i = 0; i < flowerbed.Length; i++)
         {
-            if (flowerbed[i] == 0 
-                && (i < flowerbed.Length && flowerbed[i+1] == 0) 
-                && (i > 0 && flowerbed[i-1] == 0))
+            if (flowerbed[i] == 0 && 
+                (i == flowerbed.Length - 1 || flowerbed[i+1] == 0) && (i == 0 || flowerbed[i-1] == 0))
             {
                 flowerbed[i] = 1;
                 n--;
