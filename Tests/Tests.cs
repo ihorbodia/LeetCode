54f4f8a9e8e2ea6b215ffd2ee5834ee8cc9a7c66
Ihor Bodia <ihor.bodia@dataart.com>
2023-07-18 20:05:20 +0200

    345. Reverse Vowels of a String

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -3,16 +3,16 @@ namespace Tests;
 public class Tests
 {
     [Theory]
-    [InlineData(new[] {1,0,0,0,1}, 1, true)]
-    [InlineData(new[] {1,0,0,0,1}, 2, false)]
-    [InlineData(new[] {0,0,0,0,0,1}, 1, true)]
-    [InlineData(new[] {0,0,1,0,1}, 1, true)]
+    [InlineData(new[] { 1, 0, 0, 0, 1 }, 1, true)]
+    [InlineData(new[] { 1, 0, 0, 0, 1 }, 2, false)]
+    [InlineData(new[] { 0, 0, 0, 0, 0, 1 }, 1, true)]
+    [InlineData(new[] { 0, 0, 1, 0, 1 }, 1, true)]
     public void CanPlaceFlowers_605(int[] flowerbed, int n, bool expectedResult)
     {
         for (int i = 0; i < flowerbed.Length; i++)
         {
-            if (flowerbed[i] == 0 && 
-                (i == flowerbed.Length - 1 || flowerbed[i+1] == 0) && (i == 0 || flowerbed[i-1] == 0))
+            if (flowerbed[i] == 0 &&
+                (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0) && (i == 0 || flowerbed[i - 1] == 0))
             {
                 flowerbed[i] = 1;
                 n--;
@@ -20,7 +20,40 @@ public class Tests
         }
 
         var testResult = n <= 0;
-        
+
+        Assert.Equal(testResult, expectedResult);
+    }
+
+    [Theory]
+    [InlineData("hello", "holle")]
+    [InlineData("leetcode", "leotcede")]
+    public void ReverseVowelsOfAString_345(string s, string expectedResult)
+    {
+        var inputString = s.ToCharArray();
+        var vowels = "aeiouAEIOU";
+
+        var leftPointer = 0;
+        var rightPointer = inputString.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+            if (vowels.Contains(inputString[leftPointer]) && vowels.Contains(inputString[rightPointer]))
+            {
+                (inputString[leftPointer], inputString[rightPointer]) =
+                    (inputString[rightPointer], inputString[leftPointer]);
+                leftPointer++;
+                rightPointer--;
+            }
+            else if(!vowels.Contains(inputString[rightPointer]))
+            {
+                rightPointer--;
+            }
+            else
+            {
+                leftPointer++;
+            }
+        }
+        var testResult = new string(inputString);
         Assert.Equal(testResult, expectedResult);
     }
 }
